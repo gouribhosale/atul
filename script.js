556a879a9e94233e5c592f1a586b537f9fa8857a
gouribhosale <gouribhosale143@gmail.com>
2022-04-28 22:59:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// function showmessage(){
+//     //  document.getElementById('content').style.height="400px";
+    
+//     // document.getElementById ('content').style.display=" none"    
+//     // document.getElementById('show').style.display="block"
+
+
+//     document.getElementById ('content').style.display=" none"
+//     document.getElementById('content').style.height="400px"
+//     // document.getElementById('content').style.height="400px";
+//     // document.getElementById('content').style.display="none"
+//     // document.getElementById ('show').style.display=" none"    
+
+   
+// }
+// function showmsg(){
+    
+
+
+//     document.getElementById ('content').style.display=" none"
+//      document.getElementById('content').style.height="400px"
+//    "    
+
+   
+// }
+
+
+
+
+
+
